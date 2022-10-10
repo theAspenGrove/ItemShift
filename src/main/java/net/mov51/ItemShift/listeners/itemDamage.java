49d42ac846c0cf20c1437faea8d14089fb190f7d
mov <mov51@mov51.net>
2022-10-10 07:02:36 -0400

    Change level cost to 1 point and units to points

--- src/main/java/net/mov51/ItemShift/listeners/itemDamage.java
+++ src/main/java/net/mov51/ItemShift/listeners/itemDamage.java
@@ -1,13 +1,12 @@
 package net.mov51.ItemShift.listeners;
 
-import net.mov51.ItemShift.util.Level;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 
-import static net.mov51.ItemShift.util.HoldingGold.isHoldingGold;
+import static net.mov51.ItemShift.util.HoldingGold.isItemGold;
 import static net.mov51.ItemShift.util.configHelper.levelCost;
 
 public class itemDamage implements Listener {
@@ -15,22 +14,11 @@ public class itemDamage implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onBreak(PlayerItemDamageEvent e) {
         Player p = e.getPlayer();
-        if(isHoldingGold(p)) {
-            Level levelProgress = new Level(p.getLevel(), p.getExp());
-            if (levelProgress.getLevel() > 0) {
-                if (levelProgress.getProgress() > levelCost) {
-                    levelProgress.setProgress(levelProgress.getProgress() - levelCost);
-                } else {
-                    levelProgress.setProgress(levelProgress.getProgress() + (1.0f - levelCost));
-                    if(levelProgress.getLevel() < 1){
-                        return;
-                    }
-                    levelProgress.setLevel(levelProgress.getLevel() - 1);
-                }
-                p.setExp(levelProgress.getProgress());
-                p.setLevel(levelProgress.getLevel());
-                //cancel item damage
-                e.setCancelled(true);
+            if(isItemGold(e.getItem())){
+                if (p.getLevel() > 0) {
+                    p.giveExp(-levelCost);
+                    //cancel item damage
+                    e.setCancelled(true);
                 }
             }
         }
--- src/main/java/net/mov51/ItemShift/util/configHelper.java
+++ src/main/java/net/mov51/ItemShift/util/configHelper.java
@@ -6,11 +6,11 @@ import org.bukkit.configuration.file.FileConfiguration;
 public class configHelper {
     public static FileConfiguration c;
     public static String pluginPrefix;
-    public static float levelCost;
+    public static int levelCost;
 
     public static void loadConfig(){
         c = ItemShift.plugin.getConfig();
-        levelCost = (c.getInt("level-cost") != 0 ? c.getInt("level-cost") : 0.0556f);
+        levelCost = (c.getInt("level-cost") != 0 ? c.getInt("level-cost") : 1);
         pluginPrefix = c.getString("chat-prefix") != null ? c.getString("chat-prefix") : "§8[§6ItemShift§8]§r ";
 
     }
